eae6427de4f5c3e76ed4a503b1bca5ddfb2b8259
fandis <great_beluluk@yahoo.com>
2021-10-16 11:34:25 +0700

    Menambahkan UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder {
@@ -11,6 +12,9 @@ class UserSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        //
+        User::insert([
+            ['username'=>'admin', 'password'=>User::hashPassword('admin'), 'email'=>'admin@admin.com', 'jwt'=>''],
+            ['username'=>'fandi', 'password'=>User::hashPassword('fandi'), 'email'=>'fandi@admin.com', 'jwt'=>''],
+        ]);
     }
 }
